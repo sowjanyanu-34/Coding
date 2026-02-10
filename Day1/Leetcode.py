dda3b96100df14012325a30d95bb8beb29959a14
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-02-10 21:27:11 +0530

    Day 1: Solved Two Sum II using two pointers

--- Day1/Leetcode.py
+++ Day1/Leetcode.py
@@ -24,3 +24,16 @@ class Solution:
             if complement in seen:
                 return [seen[complement], i]
             seen[num] = i
+
+#Two Sum II - Input Array Is Sorted (Leetcode 167)
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left, right = 0, len(numbers) - 1
+        while left < right:
+            curr_sum = numbers[left] + numbers[right]
+            if curr_sum == target:
+                return [left + 1, right + 1]  # 1-indexed
+            elif curr_sum < target:
+                left += 1
+            else:
+                right -= 1
