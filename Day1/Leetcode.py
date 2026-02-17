1ee030ba6f4cb660e24d3535ccc0832a384606b8
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-02-17 20:48:59 +0530

    Day 1: Solved move zeros  using two pointers

--- Day1/Leetcode.py
+++ Day1/Leetcode.py
@@ -38,3 +38,16 @@ class Solution:
                 l+=1
             else:
                 r-=1
+
+# Move Zeros leetcode 283
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        insertPos = 0
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                if i != insertPos:
+                    nums[i], nums[insertPos] = nums[insertPos], nums[i]
+                insertPos += 1
+
+                
