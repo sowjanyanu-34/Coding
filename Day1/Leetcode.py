402312155a869bb10fef4928a702fbb9ae3e95ce
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-02-21 20:58:12 +0530

    git rebase --continuellid Palindrome || Leetcode 680

--- Day1/Leetcode.py
+++ Day1/Leetcode.py
@@ -50,4 +50,24 @@ class Solution:
                     nums[i], nums[insertPos] = nums[insertPos], nums[i]
                 insertPos += 1
 
+# leetcode 680 Valid Palindrome II
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            if s[left] != s[right]:
+                return self.isPalindrome(s, left + 1, right) or self.isPalindrome(s, left, right - 1)
+            left += 1
+            right -= 1
+
+        return True
+
+    def isPalindrome(self, s: str, left: int, right: int) -> bool:
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
                 
