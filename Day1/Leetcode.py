330868d9328bf18eeacaaceb36dc7e3185404637
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-02-22 18:56:21 +0530

    Updated Day1 Leetcode solution

--- Day1/Leetcode.py
+++ Day1/Leetcode.py
@@ -71,3 +71,18 @@ class Solution:
             right -= 1
         return True
                 
+# Sort Colors (Leetcode 75)
+class Solution:    
+    def sortColors(self, nums: List[int]) -> None:
+        low, mid, high = 0, 0, len(nums) - 1
+
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:  # nums[mid] == 2
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
