95ed39f14cc86415636f80389a285ad578cbef82
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-02-24 08:45:31 +0530

    Added leetcode 11 problem

--- Day1/Leetcode.py
+++ Day1/Leetcode.py
@@ -86,3 +86,19 @@ class Solution:
             else:  # nums[mid] == 2
                 nums[mid], nums[high] = nums[high], nums[mid]
                 high -= 1
+
+# Container With Most Water (Leetcode 11)
+class Solution:
+    def maxArea(self,height:List[int])->int:
+        l=0
+        r=len(height)-1
+        max_area=0
+        while l<r:
+            width=r-l
+            h=min(height[l],height[r])
+            area=h*width
+            max_area=max(max_area,area)
+            if height[l]<height[r]:
+                l+=1
+            else:
+                r-=1
