54a02df7bbb809efcf85799bc8f0f90e2e5fb456
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-03-02 23:11:59 +0530

    leetcode 1423 modified

--- Day1/Leetcode.py
+++ Day1/Leetcode.py
@@ -120,7 +120,7 @@ class Solution:
             r-=1
         return True
     
-# leetcode 1423 Maximum Points You Can Obtain from Cards
+# leetcode 1423 Maximum Points You Can Obtain from Cards:
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
         n=len(cardPoints)
@@ -128,7 +128,7 @@ class Solution:
         rsum=0
         maxsum=0
         for i in range(k):
-            lsum=cardPoints[i]
+            lsum+=cardPoints[i]
         maxsum=lsum
         rindex=n-1
         for i in range(k-1,-1,-1):
