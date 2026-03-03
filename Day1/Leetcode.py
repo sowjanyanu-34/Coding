80dbd239afbb33b3817457d4a88bfe0f8e273917
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-03-03 20:27:34 +0530

    904 leetcode is added

--- Day1/Leetcode.py
+++ Day1/Leetcode.py
@@ -137,3 +137,19 @@ class Solution:
             rindex-=1
             maxsum=max(maxsum,lsum+rsum)
         return maxsum
+
+#leetcode 904 Fruit Into Baskets
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        count={}
+        l=0
+        max_fruits=0
+        for r in range(len(fruits)):
+            count[fruits[r]]=count.get(fruits[r],0)+1
+            while len(count)>2:
+                count[fruits[l]]-=1
+                if count[fruits[l]]==0:
+                    del count[fruits[l]]
+                l+=1
+            max_fruits=max(max_fruits,r-l+1)
+        return max_fruits
