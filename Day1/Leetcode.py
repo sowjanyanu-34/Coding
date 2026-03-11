14a80451b4c44eb281013fee9c2436d94c054eda
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-03-11 21:52:32 +0530

    Leetcode 344  is  modified

--- Day1/Leetcode.py
+++ Day1/Leetcode.py
@@ -304,3 +304,12 @@ class Solution:
         for i in range(2,n+1):
             a,b=b,a+b
         return b
+    
+# 344 Reverse String
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+      l,r=0,len(s)-1
+      while l<r:
+          s[l],s[r]=s[r],s[l]
+          l+=1
+          r-=1
