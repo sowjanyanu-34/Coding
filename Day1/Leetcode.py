89e9197096c72fcac3a3a8a3ada731eabf9bc8b1
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-03-18 21:05:01 +0530

    Leetcode 303 added

--- Day1/Leetcode.py
+++ Day1/Leetcode.py
@@ -448,3 +448,14 @@ class Solution:
             nums[i]=nums[i]+nums[i-1]
         return nums
  
+ #303 leetcode Range Sum Query - Immutable
+class NumArray:
+    def __init__(self, nums: List[int]):
+        self.prefix=[0]
+        for num in nums:
+            self.prefix.append(self.prefix[-1]+num)
+        
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.prefix[right+1]-self.prefix[left]
+        
