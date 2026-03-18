b8ab6bf551e84c7c7f1acbd13e7c48f1e4e15a85
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-03-18 21:09:56 +0530

    Leetcode added

--- Day1/Leetcode.py
+++ Day1/Leetcode.py
@@ -455,7 +455,6 @@ class NumArray:
         for num in nums:
             self.prefix.append(self.prefix[-1]+num)
         
-
     def sumRange(self, left: int, right: int) -> int:
         return self.prefix[right+1]-self.prefix[left]
         
