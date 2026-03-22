16a851abaa6c407d4474bb8becce90da3e9e8193
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-03-22 20:06:53 +0530

    Leetcode   added

--- Day1/Leetcode.py
+++ Day1/Leetcode.py
@@ -105,7 +105,7 @@ class Solution:
                 r-=1
         return max_area
     
-#Valid Palindrome (Leetcode 125):
+#Valid Palindrome (Leetcode 125)::
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         filter=[]
