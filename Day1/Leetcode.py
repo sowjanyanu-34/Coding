f2cbe8f4cd96da1a48bace042e7b62e7145db885
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-03-22 21:46:39 +0530

    Leetcode 525  added

--- Day1/Leetcode.py
+++ Day1/Leetcode.py
@@ -471,3 +471,19 @@ class Solution:
             prefix_map[curr_sum]=prefix_map.get(curr_sum,0)+1
         return count
 
+# 525 leetcode Contiguous Array
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        curr_sum=0
+        max_len=0
+        prefix_map={0:1}
+        for i in range(len(nums)):
+            if nums[i]==0:
+                curr_summ+=1
+            else:
+                curr_sum-=1
+            if curr_sum in prefix_map:
+                max_len=max(max_len,i-prefix_map[curr_sum])
+            else:
+                prefix_map[curr_sum]=i
+        return max_len
