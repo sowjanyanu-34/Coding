6b9cd0aed31a1fe259da99c3f2751c5577c5e1d7
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-04-11 16:26:33 +0530

    Leetcode 1 in java added

--- Day1/leetcode.java
+++ Day1/leetcode.java
@@ -86,3 +86,26 @@ class Solution4 {
         return count;
     }
 }
+
+// Longest Consecutive Sequence leetcode 128
+class Solution5 {
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        int longest = 0;
+        for (int num : set) {
+            if (!set.contains(num - 1)) {
+                int count = num;
+                int current = 1;
+                while (set.contains(current + 1)) {
+                    current++;
+                    count++;
+                }
+                longest = Math.max(longest, count);
+            }
+        }
+        return longest;
+    }
+}
