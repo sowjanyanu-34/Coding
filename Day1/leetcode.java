043b8dde2f5ed8a562c252b1037ea8a9449ab788
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-04-14 20:35:06 +0530

    Leetcode 1 in java added

--- Day1/leetcode.java
+++ Day1/leetcode.java
@@ -115,15 +115,15 @@ class Solution6 {
     public int lengthOfLongestSubstring(String s) {
         HashSet<Character> set = new HashSet<>();
         int left = 0;
-        int longest = 0;
+        int max_longest = 0;
         for (int right = 0; right < s.length(); right++) {
             while (set.contains(s.charAt(right))) {
                 set.remove(s.charAt(left));
                 left++;
             }
             set.add(s.charAt(right));
-            longest = Math.max(longest, right - left + 1);
+            max_longest = Math.max(max_longest, right - left + 1);
         }
-        return longest;
+        return max_longest;
     }
 }
