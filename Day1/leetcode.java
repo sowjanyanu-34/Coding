fec6c168bd1ab45ca7c7011bb874a7426d6d8f6b
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-04-15 20:01:07 +0530

    Leetcode 1 in java added

--- Day1/leetcode.java
+++ Day1/leetcode.java
@@ -158,3 +158,22 @@ class Solution7 {
 
 // Binary Search Pattern
 // Binary Search (Leetcode 704)
+class Solution8 {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+}
