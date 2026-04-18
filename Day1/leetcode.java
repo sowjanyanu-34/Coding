20028ff8f0698e478b639db249171cc9c530ef91
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-04-18 21:56:52 +0530

    Leetcode 1 in java added

--- Day1/leetcode.java
+++ Day1/leetcode.java
@@ -216,3 +216,20 @@ class Solution10 {
         return ans;
     }
 }
+
+// (leetcode 153)
+class Solution11 {
+    public int findMin(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] > nums[r]) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+        return nums[l];
+    }
+}
