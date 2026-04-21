416b9a80013b4639c6a9995050a0067b3348506c
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-04-21 23:00:13 +0530

    Leetcode 1 in java added

--- Day1/Solution10.java
+++ Day1/Solution10.java
@@ -318,3 +318,22 @@ class Solution14 {
         return left;
     }
 }
+
+//LEETCODE 189 Rotate Array
+class Solution15 {
+    public void rotate(int[] nums, int k) {
+    k=k.nums.length;
+    reverse(nums,0,n-1);
+    reverse(nums,0,k-1);
+    reverse(nums,k,n-1);
+    }
+    private void reverse(int[] nums,int start,int end){
+        while(start<end){
+            int temp=nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            start++;    
+            end--;
+        }
+    }
+}
