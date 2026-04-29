036581a814d4e52bbacde0cf2eed05a5840c2b35
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-04-29 18:22:59 +0530

    Leetcode 1 in java added

--- Day1/Solution10.java
+++ Day1/Solution10.java
@@ -639,3 +639,29 @@ class Solution23 {
         System.out.println(findPages(arr, k));
     }
 }
+
+// 74 leetcode Search in 2D matrix
+class Solution24 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
+            return false;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int l = 0;
+        int r = m * n - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            int row = mid / n;
+            int col = mid % n;
+            int value = matrix[row][col];
+            if (value == target) {
+                return true;
+            } else if (value < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return false;
+    }
+}
