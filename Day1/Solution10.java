cdcadc29840022fb208c2ba95f20bbf994752113
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-05-01 10:56:11 +0530

    Leetcode 1 in java added

--- Day1/Solution10.java
+++ Day1/Solution10.java
@@ -718,3 +718,20 @@ class Solution27 {
         return n * factorial(n - 1);
     }
 }
+
+// 509 Fibonacci Number
+class Solution28 {
+    public int fib(int n) {
+        if (n <= 1)
+            return n;
+        int a = 0;
+        int b = 1;
+        for (int i = 2; i <= n; i++) {
+            int sum = a + b;
+            a = b;
+            b = sum;
+        }
+        return b;
+
+    }
+}
