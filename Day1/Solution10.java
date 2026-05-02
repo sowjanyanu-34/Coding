121769f9e70da26393ade4c37bcc68f42938beeb
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-05-02 19:09:33 +0530

    Leetcode 1 in java added

--- Day1/Solution10.java
+++ Day1/Solution10.java
@@ -875,3 +875,19 @@ class Solution34 {
         return stack.isEmpty();
     }
 }
+
+// 1047 leetcode Remove All Adjacent Duplicates In String
+class Solution35 {
+    public String removeDuplicates(String s) {
+        StringBuilder st = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            int n = st.length();
+            if (n > 0 && st.charAt(n - 1) == ch) {
+                st.deleteCharAt(n - 1);
+            } else {
+                st.append(ch);
+            }
+        }
+        return st.toString();
+    }
+}
