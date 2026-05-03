766b74d8d3bc22edf4d3f3099e2a45bbed479466
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-05-03 21:57:48 +0530

    Leetcode 1 in java added

--- Day1/Solution10.java
+++ Day1/Solution10.java
@@ -1048,3 +1048,23 @@ class Solution37 {
         return result;
     }
 }
+
+// 503 leetcode Next Greater Element II
+class Solution38 {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        Arrays.fill(res, -1);
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < 2 * n; i++) {
+            int num = nums[i % n];
+            while (!stack.isEmpty() && nums[stack.peek()] < num) {
+                res[stack.pop()] = num;
+            }
+            if (i < n) {
+                stack.push(i);
+            }
+        }
+        return res;
+    }
+}
