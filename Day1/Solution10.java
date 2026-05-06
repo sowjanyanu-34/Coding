36f91b7c5f60721eb0eb3c74212bc641fd45297a
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-05-06 22:13:03 +0530

    Leetcode 1 in java added

--- Day1/Solution10.java
+++ Day1/Solution10.java
@@ -1115,3 +1115,43 @@ class StockSpanner40 {
         return span;
     }
 }
+
+// leetcode 150 Evaluate Reverse Polish Notation
+
+// 394 leetcode Decode String
+class Solution41 {
+    public String decodeString(String s) {
+        Stack<Integer> countStack = new Stack<>();
+        Stack<String> stringStack = new Stack<>();
+
+        String curr = "";
+        int num = 0;
+
+        for (char ch : s.toCharArray()) {
+
+            if (Character.isDigit(ch)) {
+                num = num * 10 + (ch - '0');
+            } else if (ch == '[') {
+                countStack.push(num);
+                stringStack.push(curr);
+
+                num = 0;
+                curr = "";
+            } else if (ch == ']') {
+                int repeat = countStack.pop();
+                String prev = stringStack.pop();
+
+                StringBuilder temp = new StringBuilder(prev);
+                for (int i = 0; i < repeat; i++) {
+                    temp.append(curr);
+                }
+
+                curr = temp.toString();
+            } else {
+                curr += ch;
+            }
+        }
+
+        return curr;
+    }
+}
