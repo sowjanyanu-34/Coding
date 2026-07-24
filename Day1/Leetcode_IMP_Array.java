27bee800e22fe266a035e0f7d8829b09e1e3fd31
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-07-24 18:35:04 +0530

    Leetcode added

--- Day1/Leetcode_IMP_Array.java
+++ Day1/Leetcode_IMP_Array.java
@@ -42,3 +42,22 @@ class Solution {
         }
     }
 }
+
+// leetcode 167
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;   
+            }
+        }
+        return new int[]{};
+    }
+} 
