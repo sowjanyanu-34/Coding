95b294153049fa132bbaadbaba0bce231432451e
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-07-25 11:57:24 +0530

    Leetcode added

--- Day1/leetcode.sql
+++ Day1/leetcode.sql
@@ -1,5 +1,26 @@
+#175 Leetcode
+SELECT 
+p.firstname,
+p.lastname,
+a.city,
+a.state
+FROM Person p
+INNER JOIN 
+Address a
+ON p.personId=a.personId;
+
+#Leetcode 181 
+SELECT e.name AS Employee
+FROM Employee e
+JOIN Employee m
+ON e.managerId = m.id
+WHERE e.salary > m.salary;
+
 # Leetcode 182 duplicate Emails:
 SELECT email as Email
 FROM person
 GROUP BY email
 HAVING COUNT(email)>1; 
+
+
+
