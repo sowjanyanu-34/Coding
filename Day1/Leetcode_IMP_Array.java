d8dc80791af8b6500077816e092f9cceb1f0fbe0
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-07-25 18:31:58 +0530

    Leetcode added

--- Day1/Leetcode_IMP_Array.java
+++ Day1/Leetcode_IMP_Array.java
@@ -179,3 +179,20 @@ class Solution7 {
         return water;
     }
 }
+
+// Sliding window
+// leetcode 643
+class Solution8 {
+    public double findMaxAverage(int[] nums, int k) {
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        int maxSum = sum;
+        for (int i = k; i < nums.length; i++) {
+            sum = sum - nums[i - k] + nums[i];
+            maxSum = Math.max(maxSum, sum);
+        }
+        return (double) maxSum / k;
+    }
+}
