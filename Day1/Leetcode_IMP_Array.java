816c8c38e754b065d96fb82e6b5e28a779b06bcf
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-07-25 18:48:00 +0530

    Leetcode added

--- Day1/Leetcode_IMP_Array.java
+++ Day1/Leetcode_IMP_Array.java
@@ -196,3 +196,24 @@ class Solution8 {
         return (double) maxSum / k;
     }
 }
+
+// leetcode 1343
+class Solution9 {
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+        int count = 0;
+        if (sum >= k * threshold) {
+            count++;
+        }
+        for (int i = k; i < arr.length; i++) {
+            sum = sum - arr[i - k] + arr[i];
+            if (sum >= k * threshold) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
