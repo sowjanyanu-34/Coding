fce58a8aa4d21562d45c999f6062ba280c56f6c2
sowjanyanu-34 <sowjanyanu34@gmail.com>
2026-07-25 21:15:22 +0530

    Leetcode added

--- Day1/Leetcode_IMP_Array.java
+++ Day1/Leetcode_IMP_Array.java
@@ -242,3 +242,24 @@ class Solution10 {
         return minLength == Integer.MAX_VALUE ? 0 : minLength;
     }
 }
+
+// leetcode 485
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+
+        int count = 0;
+        int max = 0;
+
+        for (int num : nums) {
+
+            if (num == 1) {
+                count++;
+                max = Math.max(max, count);
+            } else {
+                count = 0;
+            }
+        }
+
+        return max;
+    }
+}
